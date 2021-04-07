582e63fdf079dd6e232c0c02357ec512c22159b3
Efrem Abrehe <35783634+EfremBAK@users.noreply.github.com>
2021-04-07 01:38:51 -0500

    lab9

--- Lab9_HW/behave.js
+++ Lab9_HW/behave.js
@@ -9,6 +9,35 @@ let Person = {
     }
 }
 let john = Object.create(Person);
-john.name = "John";
-john.dob = new Date(2012/12/12);
-console.log(john.dob)
+john.setName ("John");
+john.dob = new Date(1998,12,10);
+console.log(`the person's name is ${john.name}, ${john.name} was born on ${john.dob.getYear()}`);
+
+
+
+
+//employee
+//q2
+const employee = Object.create(Person);
+employee.salary = 0;
+employee.hireDate = new Date();
+employee.doJob = function(title){
+    console.log(`${this.name} is a ${title}, and earns ${this.salary}`);
+}
+const anna = Object.create(employee);
+anna.setName("Anna");
+anna.salary = 249995.50;
+anna.doJob("programmer");
+
+
+//q3
+function Persons(name, dob){
+    this.name = name;
+    this.dob = dob;
+}
+const peter = new Persons("Peter", new Date(1985,10,12));
+Persons.prototype.toString = function(){
+    return `Name: ${this.name}, DOB: ${new Intl.DateTimeFormat().format( this.dob)}`;
+}
+
+console.log(peter.toString());
