cf117ef00a83f97ed4f027ecf78f6e92550c5ce1
Efrem Abrehe <35783634+EfremBAK@users.noreply.github.com>
2021-04-09 02:25:27 -0500

    lab11

--- minibankapp/minibankapp/js/minibankapp.js
+++ minibankapp/minibankapp/js/minibankapp.js
@@ -2,46 +2,40 @@
  * minibankapp.js
  */
 "use strict";
-$( function() {
-    let currentRowId = 2;
-   $("#formNewAccount").on
-  ("submit", 
-        event => {
-            event.preventDefault();
-            const txtAccountNo = $("#txtAccountNo").val();
-            const txtCustomerName = $("#txtCustomerName").val();
-            const ddlAccountType =$("#ddlAccountType").val();
-           
-            const tables = $("#tblAccounts");
-            const newRecord = tables.find('tbody')
-            .append(`<tr><td>${++currentRowId}</td><td>${txtAccountNo}</td><td>${txtCustomerName}</td><td>${ddlAccountType}</td></tr>`);
-            tables.show().append(newRecord);
-           
-    });
+$(function () {
+  let currentRowId = 2;
+  $("#formNewAccount").on("submit", (event) => {
+    event.preventDefault();
+    const txtAccountNo = $("#txtAccountNo").val();
+    const txtCustomerName = $("#txtCustomerName").val();
+    const ddlAccountType = $("#ddlAccountType").val();
 
-    
-    $("#savings_checkbox").change(
-        function() {
-            if($(this).prop("checked") == true){
-                $("tbody").find("tr").each(
-                    function() {
-                        let isSaving = $(this).find(".isSaving").html() === "Yes";
-                        if(!isSaving) {
-                            $(this).hide();
-                        }
-                    }
-                );
-            }
-            else if($(this).prop("checked") == false){
-                $("tbody").find("tr").each(
-                    function() {
-                        let isSaving = $(this).find(".isSaving").html() === "Yes";
-                        if(!isSaving) {
-                            $(this).show();
-                        }
-                    }
-                );
-            }
-        }
-    );
+    const tables = $("#tblAccounts");
+    const newRecord = tables
+      .find("tbody")
+      .append(
+        `<tr><td>${++currentRowId}</td><td>${txtAccountNo}</td><td>${txtCustomerName}</td><td>${ddlAccountType}</td></tr>`
+      );
+    tables.show().append(newRecord);
+  });
+
+  $("#savings_checkbox").change(function () {
+    if ($(this).val() == "checked") {
+      // ddlAccountType === '_Savings'
+      $("tbody")
+        .find("tr")
+        .each(function () {
+          let savacct = $(this).find("Savings");
+          if (!savacct) {
+            $(this).hide();
+          }
+          else{$(this).show()}
+        });
+    }
+  });
+
+//   $("#savings_checkbox").change(function(){
+//     $(this).prop("checked").toggle(!this.checked); 
+// });
+
 });
